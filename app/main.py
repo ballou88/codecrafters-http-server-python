0b712b81d78b700d375cbc7e3098beb0ff5bc8bc
Mike Ballou <mike@mikeballou.com>
2024-09-13 13:01:11 -0700

    Initial implementation returning a 200

--- app/main.py
+++ app/main.py
@@ -8,7 +8,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client, address = server_socket.accept()
+
+    print(f"New connection from {address}")
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    client.send(response.encode("utf-8"))
+    print("Response Sent")
+
+    client.close()
 
 
 if __name__ == "__main__":
