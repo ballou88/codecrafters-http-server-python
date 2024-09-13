6b968d61ace055a754d84cf96b812e23874424d0
Mike Ballou <mike@mikeballou.com>
2024-09-13 16:20:05 -0700

    Implemented user-agent endpoint and refactored

--- app/main.py
+++ app/main.py
@@ -1,49 +1,58 @@
 import socket
 from typing import List  # noqa: F401
+import concurrent.futures
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    CLRF = "\r\n"
     HOST = "localhost"
     PORT = 4221
-
-    # Uncomment this to pass the first stage
-    #
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        connection, address = s.accept()
-        with connection:
-            while True:
-                print(f"New connection from {address}")
-                data = connection.recv(1024).decode("utf-8")
-                request_data = data.split(CLRF)
-                request = request_data[0]
-                headers = request_data[1:-1]
-                print(f"Recieved request: {request}")
-                request_parts = request.split()
-                path = request_parts[1]
-                if path == "/":
-                    connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
-                elif path == "/user-agent":
-                    response = generate_user_agent_response(headers)
-                    connection.send(response.encode())
-                elif path.startswith("/echo/"):
-                    response = generate_echo_response(path)
-                    connection.send(response.encode())
-                else:
-                    connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        while True:
+            conn, addr = server_socket.accept()
+            print(f"New connection from {addr}")
+            executor.submit(handle_connection, conn)
 
 
-def generate_user_agent_response(headers: List):
-    for header in headers:
-        if header.startswith("User-Agent:"):
-            string = header.split()[1]
-            length = len(string)
-            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{string}"
-    return ""
+def handle_connection(conn):
+    print("handling connection")
+    data = conn.recv(1024).decode()
+    req = parse_request(data)
+    print(f"Recieved request: {req}")
+    if req["path"] == "/":
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif req["path"] == "/user-agent":
+        response = generate_user_agent_response(req["headers"])
+        conn.send(response.encode())
+    elif req["path"].startswith("/echo/"):
+        response = generate_echo_response(req["path"])
+        conn.send(response.encode())
+    else:
+        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+
+def parse_request(data):
+    CLRF = "\r\n"
+    output = {"method": "", "path": "", "version": "", "headers": {}, "body": ""}
+    lines = data.split(CLRF)
+    request_list = lines[0].split()
+    output["method"] = request_list[0]
+    output["path"] = request_list[1]
+    output["version"] = request_list[2]
+    # Parse headers
+    index = 1
+    while lines[index] != "":
+        header = lines[index].split(":")
+        output["headers"][header[0]] = header[1].lstrip()
+        index += 1
+    output["body"] = lines[index + 1]
+    print(output)
+    return output
+
+
+def generate_user_agent_response(headers):
+    length = len(headers["User-Agent"])
+    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{headers['User-Agent']}"
 
 
 def generate_echo_response(path):
