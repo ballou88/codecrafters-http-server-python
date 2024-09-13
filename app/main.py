b112e0b893a10e4f5d8dadaa04e8d83b539ad18d
Mike Ballou <mike@mikeballou.com>
2024-09-13 14:14:42 -0700

    Implemented user-agent endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-import socket  # noqa: F401
+import socket
+from typing import List  # noqa: F401
 
 
 def main():
@@ -20,19 +21,33 @@ def main():
                 data = connection.recv(1024).decode("utf-8")
                 request_data = data.split(CLRF)
                 request = request_data[0]
+                headers = request_data[1:-1]
                 print(f"Recieved request: {request}")
                 request_parts = request.split()
-                if request_parts[1] == "/":
+                path = request_parts[1]
+                if path == "/":
                     connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
-                elif request_parts[1].startswith("/echo/"):
-                    response = generate_echo_response(request_parts[1])
+                elif path == "/user-agent":
+                    response = generate_user_agent_response(headers)
+                    connection.send(response.encode())
+                elif path.startswith("/echo/"):
+                    response = generate_echo_response(path)
                     connection.send(response.encode())
                 else:
                     connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
-def generate_echo_response(request):
-    string = request.split("/")[2]
+def generate_user_agent_response(headers: List):
+    for header in headers:
+        if header.startswith("User-Agent:"):
+            string = header.split()[1]
+            length = len(string)
+            return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{string}"
+    return ""
+
+
+def generate_echo_response(path):
+    string = path.split("/")[2]
     length = len(string)
     return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{string}"
 
