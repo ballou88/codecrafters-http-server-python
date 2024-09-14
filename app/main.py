a05b99021ddc2f851260bf6a665f59d850eb5c0e
Mike Ballou <mike@mikeballou.com>
2024-09-14 15:44:34 -0700

    Refactored everything to use reposonse class

--- app/main.py
+++ app/main.py
@@ -30,47 +30,35 @@ def handle_request(req):
     # if req is None:
     #     response.status = 500
     #     return response.out()
-    res_data = {}
     if req.method == "GET":
         if req.path == "/":
             response.status = 200
-            return response.out()
         elif req.path == "/user-agent":
             response.status = 200
-            response.body = req.headers["User-Agent"].encode()
+            response.body = req.headers["User-Agent"]
             response.content_type = "text/plain"
-            return response.out()
         elif req.path.startswith("/echo/"):
-            res_data = generate_echo_response(req)
+            string = req.path.split("/")[2]
+            if "Accept-Encoding" in req.headers:
+                response.accept_encoding = req.headers["Accept-Encoding"]
+            response.status = 200
+            response.content_type = "text/plain"
+            response.body = string
         elif req.path.startswith("/files/"):
-            res_data = generate_file_response(req)
+            data = load_file(req)
+            if data:
+                response.status = 200
+                response.content_type = "application/octet-stream"
+                response.body = data
+            else:
+                response.status = 404
         else:
             response.status = 404
-            return response.out()
     elif req.method == "POST":
         if req.path.startswith("/files/"):
             handle_file_create(req)
             response.status = 201
-            return response.out()
-    return build_response(res_data)
-
-
-def build_response(res_data):
-    RESPONSE = {200: "200 OK", 201: "201 Created", 404: "404 Not Found"}
-    response = [f"HTTP/1.1 {RESPONSE[res_data['status']]}"]
-    if "Content-Type" in res_data:
-        response.append(f"Content-Type: {res_data['Content-Type']}")
-    if "Content-Length" in res_data:
-        response.append(f"Content-Length: {res_data['Content-Length']}")
-    if "Content-Encoding" in res_data:
-        response.append(f"Content-Encoding: {res_data['Content-Encoding']}")
-    out = "\r\n".join(response) + "\r\n\r\n"
-    if res_data.get("body"):
-        out = out.encode() + bytes(res_data["body"])
-    else:
-        out = out.encode()
-    print(out)
-    return out
+    return response.out()
 
 
 def handle_file_create(req):
@@ -83,7 +71,7 @@ def handle_file_create(req):
         f.write(req.body)
 
 
-def generate_file_response(req):
+def load_file(req):
     file_name = req.path.split("/")[2]
     parser = argparse.ArgumentParser()
     parser.add_argument("--directory", dest="directory")
@@ -92,33 +80,9 @@ def generate_file_response(req):
     if os.path.isfile(file_path):
         with open(file_path, "r") as f:
             data = f.read()
-        res_data = {
-            "status": 200,
-            "Content-Type": "application/octet-stream",
-            "body": data.encode(),
-            "Content-Length": len(data),
-        }
-        return res_data
+        return data
     else:
-        return {"status": 404}
-
-
-def generate_echo_response(req):
-    string = req.path.split("/")[2]
-    res_data = {
-        "status": 200,
-        "Content-Type": "text/plain",
-        "body": string.encode(),
-        "Content-Length": len(string.encode()),
-    }
-    if "Accept-Encoding" in req.headers:
-        encodings = req.headers["Accept-Encoding"]
-        for encoding in encodings:
-            if encoding in ["gzip"]:
-                res_data["body"] = gzip.compress(string.encode())
-                res_data["Content-Encoding"] = encoding
-                res_data["Content-Length"] = len(res_data["body"])
-    return res_data
+        return None
 
 
 if __name__ == "__main__":
--- app/response.py
+++ app/response.py
@@ -1,3 +1,6 @@
+import gzip
+
+
 class Response:
     RESPONSE = {200: "200 OK", 201: "201 Created", 404: "404 Not Found"}
 
@@ -5,17 +8,23 @@ class Response:
         self.status = 500
         self.content_type = ""
         self.content_encoding = ""
-        self.body = None
+        self.body = ""
+        self.accept_encoding = []
+        self.encoded_body = ""
 
     def out(self):
         response = [f"HTTP/1.1 {Response.RESPONSE[self.status]}"]
+        if self.accept_encoding:
+            for encoding in self.accept_encoding:
+                if encoding in ["gzip"]:
+                    self.body = gzip.compress(self.body.encode())
+                    self.content_encoding = encoding
         response.append(f"Content-Type: {self.content_type}")
         response.append(f"Content-Encoding: {self.content_encoding}")
-        if self.body:
-            response.append(f"Content-Length: {len(self.body)}")
+        response.append(f"Content-Length: {len(self.body)}")
         response = "\r\n".join(response) + "\r\n\r\n"
-        if self.body:
+        if self.content_encoding != "":
             response = response.encode() + bytes(self.body)
         else:
-            response = response.encode()
+            response = response.encode() + self.body.encode()
         return response
